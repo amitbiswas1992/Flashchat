84f4ef18451be21422b38f3f4f2e12cd5e2b6ed9
Amit Biswas <biswas1992@hotmail.com>
2018-09-03 06:13:58 -0400

    Populated table view with data

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Declare instance variables here
 
@@ -27,7 +27,8 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         
         
         //TODO: Set yourself as the delegate of the text field here:
@@ -40,6 +41,10 @@ class ChatViewController: UIViewController {
 
         //TODO: Register your MessageCell.xib file here:
 
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "CustomMessageCell")
+        
+        
+        
         
     }
 
@@ -47,15 +52,23 @@ class ChatViewController: UIViewController {
     
     //MARK: - TableView DataSource Methods
     
-    
+   
     
     //TODO: Declare cellForRowAtIndexPath here:
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomMessageCell", for: indexPath) as! CustomMessageCell
+        let messageArray = ["First Message", "Second Message ", "Third Message"]
+        cell.messageBody.text = messageArray[indexPath.row]
+        return cell
+    }
     
     
     //TODO: Declare numberOfRowsInSection here:
     
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
     //TODO: Declare tableViewTapped here:
     
